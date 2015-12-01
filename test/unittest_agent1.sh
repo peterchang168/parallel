e4ba3127bf13b184dcd197c069b5266f686a1117
Peter L Chang (RD-TW) <peterl_chang@trend.com.tw>
2015-12-01 20:00:41 +0800

    move ${PWD} inside unittest_agent.sh

--- test/unittest_agent1.sh
+++ test/unittest_agent1.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
-if [ "$#" -lt "1" ]; then
-	echo "error: not enough parameters."
-	echo "usage: $0 [workspace_dir]"
-	exit -1
-fi
-
-CWD="$1"
 
 mkdir -p /tmp/agent1_test
-cp ${CWD}/bin/agent1.py /tmp/agent1_test
-cp ${CWD}/test/unittest/unittest_agent1.py /tmp/agent1_test
+cp ${PWD}/bin/agent1.py /tmp/agent1_test
+cp ${PWD}/test/unittest/unittest_agent1.py /tmp/agent1_test
 mkdir -p /tmp/agent1_test/report
 nosetests -v -s -x --with-xunit --xunit-file=$CIRCLE_TEST_REPORTS/agent1_unit_result.xml --cover-erase --with-coverage --cover-package=agent1 -w /tmp/agent1_test/ unittest_agent1.py
 coverage xml -o $CIRCLE_TEST_REPORTS/agent1_coverage.xml /tmp/agent1_test/agent1.py
--- test/unittest_agent2.sh
+++ test/unittest_agent2.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
-if [ "$#" -lt "1" ]; then
-	echo "error: not enough parameters."
-	echo "usage: $0 [workspace_dir]"
-	exit -1
-fi
-
-CWD="$1"
-
 mkdir -p /tmp/agent2_test
-cp ${CWD}/bin/agent2.py /tmp/agent2_test
-cp ${CWD}/test/unittest/unittest_agent2.py /tmp/agent2_test
+cp ${PWD}/bin/agent2.py /tmp/agent2_test
+cp ${PWD}/test/unittest/unittest_agent2.py /tmp/agent2_test
 mkdir -p /tmp/agent2_test/report
 nosetests -v -s -x --with-xunit --xunit-file=$CIRCLE_TEST_REPORTS/agent2_unit_result.xml --cover-erase --with-coverage --cover-package=agent2 -w /tmp/agent2_test/ unittest_agent2.py
 coverage xml -o $CIRCLE_TEST_REPORTS/agent2_coverage.xml /tmp/agent2_test/agent2.py
